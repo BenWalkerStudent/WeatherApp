01d23793b5ba5f5a3631a7485c784e7bf50cd084
Freeborger <ben.and.david.walker@gmail.com>
2023-09-12 13:10:07 -0600

    a solid final commit for the section

--- script.js
+++ script.js
@@ -13,6 +13,9 @@ for (let i = 0; i < tempature.length; i++) {
 
 let articleTitle = [document.getElementById('article-link-1'), document.getElementById('article-link-2'), document.getElementById('article-link-3'), document.getElementById('article-link-4'), document.getElementById('article-link-5')]
 
+let articleDesc = [document.getElementById('article-desc-1'), document.getElementById('article-desc-2'), document.getElementById('article-desc-3'), document.getElementById('article-desc-4')]
+
+
 
 
 articleTitle[0].textContent = 'A Cute Cat'
@@ -22,4 +25,12 @@ articleTitle[1].textContent = 'Same Cat'
 articleTitle[2].textContent = 'CAN I GET A NEW CAT'
 
 articleTitle[3].textContent = 'Thank You'
+
+articleDesc[0].textContent = 'pretty cute'
+
+articleDesc[1].textContent = 'did they not have a new idea'
+
+articleDesc[2].textContent = 'why the heck is it the same'
+
+articleDesc[3].textContent = 'okay now we got a new cat, im exstatic!'
 
