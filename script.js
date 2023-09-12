e5eb8f584d317f74f2e2968191b892500ed18a98
Freeborger <ben.and.david.walker@gmail.com>
2023-09-12 10:33:49 -0600

    final commit before lunch

--- script.js
+++ script.js
@@ -11,4 +11,15 @@ for (let i = 0; i < tempature.length; i++) {
 }
 
 
+let articleTitle = [document.getElementById('article-link-1'), document.getElementById('article-link-2'), document.getElementById('article-link-3'), document.getElementById('article-link-4'), document.getElementById('article-link-5')]
+
+
+
+articleTitle[0].textContent = 'A Cute Cat'
+
+articleTitle[1].textContent = 'Same Cat'
+
+articleTitle[2].textContent = 'CAN I GET A NEW CAT'
+
+articleTitle[3].textContent = 'Thank You'
 
