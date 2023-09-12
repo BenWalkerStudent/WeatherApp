921004a012eb0fa673c222be789fc1d7c90ebd65
Freeborger <ben.and.david.walker@gmail.com>
2023-09-12 09:12:55 -0600

    adding javascript

---
+++ script.js
@@ -0,0 +1,7 @@
+let tempature = [document.getElementById('data1'), document.getElementById('data2')]
+
+let currentTemp = document.getElementById(currentTemp)
+
+currentTemp.innerHTML = '65'
+
+tempature[0].innerHTML = '89'
